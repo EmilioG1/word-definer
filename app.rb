4e544482183675c8cd6d9e74d99bf58e04a3f519
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-13 10:49:16 -0700

    add app.rb routes and erb pages

--- app.rb
+++ app.rb
@@ -4,3 +4,30 @@ require('./lib/word')
 require('pry')
 require('./lib/definition')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_words)
+end
+
+post('/words') do
+  value = params[:word_value]
+  word = Word.new(value, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
--- lib/definition.rb
+++ lib/definition.rb
@@ -4,3 +4,14 @@ require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 
+class Definition
+  @@defs = {}
+  @@id_count = 0
+
+  def initialize(value, id)
+    @value = value
+    @id = id || @id_count += 1
+  end
+
+  def
+end

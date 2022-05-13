f4bf9a2eeba54e3eebd3d22705377dfd2bedaecb
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-13 11:42:01 -0700

    add routing for words

--- app.rb
+++ app.rb
@@ -19,15 +19,27 @@ get('/words/new') do
   erb(:new_words)
 end
 
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
 post('/words') do
   value = params[:word_value]
-  word = Word.new(value, nil)
+  word = Word.new(val, nil)
   word.save()
   @words = Word.all()
   erb(:words)
 end
 
-get('/words/:id') do
+get('/words/:id/edit') do
   @word = Word.find(params[:id].to_i())
-  erb(:word)
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:val])
+  @word = Word.all
+  erb(:words)
 end

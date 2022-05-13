c2183762d5b358b356ee75046d5e5940bd51d516
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-13 11:22:25 -0700

    add definition functionality

--- lib/definition.rb
+++ lib/definition.rb
@@ -5,17 +5,48 @@ require('pry')
 also_reload('lib/**/*.rb')
 
 class Definition
-  attr_reader :value, :id
+  attr_reader :id
+  attr_accessor :val, :word_id
+
   @@definitions = {}
   @@id_count = 0
 
-  def initialize(value, id)
-    @value = value
-    @id = id || @id_count += 1
+  def initialize(val, id)
+    @val = val
+    @word_id = word_id
+    @id = id || @@id_count += 1
+  end
+
+  def word
+    Word.find(self.word_id)
   end
 
   def self.all
     @@definitions.values()
   end
+  
+  def ==(definitions_to_compare)
+    self.val() == definitions_to_compare.val() && self.id() == definitions_to_compare.id()
+  end
 
+  def save
+    @@definitions[self.id] = Definition.new(self.val, self.id)
+  end
+
+  def self.clear
+    @@definitions = {}
+    @@id_count = 0
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def delete
+    @@definitions.delete(self.id)
+  end
+
+  def update(val)
+    @val = val
+  end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,11 +3,70 @@ require 'pry'
 require 'definition'
 
 describe ('#Definition') do
+  before(:each) do
+    Definition.clear()
+  end
 
-  
   describe('.all') do
     it('returns an empty array when there are no words') do
       expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it('checks if it has the same attributes as other definitions') do
+      defs = Definition.new('hello', 1)
+      defs2 = Definition.new('hello', 1)
+      expect(defs).to(eq(defs2))
+    end
+  end
+
+  describe('#save') do
+    it('saves a definition') do
+      defs = Definition.new('caio', nil)
+      defs.save()
+      expect(Definition.all).to(eq([defs]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all definitions") do
+      defs = Definition.new("caio", nil)
+      defs.save()
+      defs2 = Definition.new("bonjour", nil)
+      defs2.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it("finds a definition by id") do
+      defs = Definition.new("caio", nil)
+      defs.save()
+      defs2 = Definition.new("bonjour", nil)
+      defs2.save()
+      expect(Definition.find(defs.id)).to(eq((defs)))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes a definition by id") do
+      defs = Definition.new("caio", nil)
+      defs.save()
+      defs2 = Definition.new("bonjour", nil)
+      defs2.save()
+      defs.delete()
+      expect(Definition.all).to(eq([defs2]))
+    end
+  end
+
+  describe('#update') do
+    it("updates a definition by id") do
+      defs = Definition.new("caio", nil)
+      defs.save()
+      defs.update("bonjour")
+      expect(defs.val).to(eq("bonjour"))
     end
   end
 end

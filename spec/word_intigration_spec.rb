30b8de2137f6789f50f0c6de16ea9be4ce5f20a1
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-26 21:53:08 -0700

    updates and completes intigration specs so all routes are passing

--- spec/word_intigration_spec.rb
+++ spec/word_intigration_spec.rb
@@ -4,6 +4,13 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('create a word path', {:type => :feature}) do
+  before(:each) do
+    Word.clear
+    Definition.clear
+    @word = Word.new("house", nil)
+    @word.save
+  end
+
   it('creates a word and then goes to the word page') do
     visit('/words')
     click_on('Add a new word')
@@ -11,15 +18,50 @@ describe('create a word path', {:type => :feature}) do
     click_on('Go!')
     expect(page).to have_content('house')
   end
+  
+  it('updates a word') do
+    visit("/words/#{@word.id}")
+    click_on('Edit word')
+    fill_in('val', :with => 'coffee')
+    click_on('Update')
+    expect(page).to have_content('coffee')
+  end
+  
+  it('deletes a word') do
+    visit("/words/#{@word.id}/edit")
+    click_on('Delete word')
+    expect(page).to have_content('')
+  end
 end
 
 describe('create a definition path', {:type => :feature}) do
-  it('creates a word and then goes to the word page') do
-    word = Word.new("house", nil)
-    word.save
-    visit("/words/#{word.id}")
-    fill_in('defs_name', :with => 'All You Need Is Love')
+  
+  before(:each) do
+    Word.clear
+    Definition.clear
+    @word = Word.new("house", nil)
+    @word.save
+    @definition = Definition.new("a humble abode", @word.id, nil)
+    @definition.save
+    end
+
+  it('creates a definition') do
+    visit("/words/#{@word.id}")
+    fill_in('defs_name', :with => 'a humble abode')
     click_on('Add definition')
-    expect(page).to have_content('All You Need Is Love')
+    expect(page).to have_content('a humble abode')
+  end
+
+  it('updates a definition') do
+    visit("/words/#{@word.id}/definitions/#{@definition.id}")
+    fill_in('val', :with => 'a place for stuff')
+    click_on('Update definition')
+    expect(page).to have_content('a place for stuff')
+  end
+
+  it('deletes a definition') do
+    visit("/words/#{@word.id}/definitions/#{@definition.id}")
+    click_on('Delete definition')
+    expect(page).to have_content('')
   end
 end
